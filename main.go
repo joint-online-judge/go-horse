8b69412cbd1784abf76ccff5246ce032918d244c
BoYanZh <boyanzh233@gmail.com>
2023-01-20 03:25:00 +0800

    feat: init

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joint-online-judge/go-horse/router"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+)
+
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
+
+// @title Fiber Example API
+// @version 1.0
+// @description This is a sample swagger for Fiber
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email fiber@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /
+func main() {
+	godotenv.Load()
+	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // comma string format
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	// database.ConnectDB()
+
+	router.Initalize(app)
+	log.Fatal(app.Listen(":" + getenv("PORT", "3000")))
+}
+
+/*
+ENV Variables:
+will auto set to 3000 if not set
+PORT=3000
+this should be a connection string or url
+DATABASE_URL="host=localhost port=5432 user=postgres password= dbname= sslmode=disable"
+**
+Docker Command for Postgres database:
+docker run --name database -d -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:alpine
+
+DB_URL Variable for docker database
+DATABASE_URL="host=localhost port=5432 user=postgres password=password dbname=postgres sslmode=disable"
+**
+Docker build base image in first stage for development
+docker build --target build -t base .
+**
+run dev container
+docker run -p 3000:3000 --mount type=bind,source=/root/go/src/fiber,target=/go/src/app --name fiber -td base
+**
+rebuild and run package
+docker exec -it web go run main.go
+**
+stop and remove container
+docker stop fiber; docker rm fiber
+*/
---
+++ router/router.go
@@ -0,0 +1,59 @@
+package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/swagger"
+	_ "github.com/joint-online-judge/go-horse/docs"
+	"github.com/joint-online-judge/go-horse/handlers"
+	"github.com/joint-online-judge/go-horse/middleware"
+)
+
+func Initalize(router *fiber.App) {
+
+	router.Get("/swagger/*", swagger.HandlerDefault) // default
+
+	router.Get("/swagger/*", swagger.New(swagger.Config{ // custom
+		URL:         "http://example.com/doc.json",
+		DeepLinking: false,
+		// Expand ("list") or Collapse ("none") tag groups by default
+		DocExpansion: "none",
+		// Prefill OAuth ClientId on Authorize popup
+		OAuth: &swagger.OAuthConfig{
+			AppName:  "OAuth Provider",
+			ClientId: "21bb4edc-05a7-4afc-86f1-2e151e4ba6e2",
+		},
+		// Ability to change OAuth2 redirect uri location
+		OAuth2RedirectUrl: "http://localhost:3000/swagger/oauth2-redirect.html",
+	}))
+
+	router.Use(middleware.Security)
+
+	router.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).SendString("Hello, World!")
+	})
+
+	router.Use(middleware.Json)
+
+	users := router.Group("/users")
+	users.Post("/", handlers.CreateUser)
+	users.Delete("/", middleware.Authenticated, handlers.DeleteUser)
+	users.Put("/", middleware.Authenticated, handlers.ChangePassword)
+	users.Post("/me", middleware.Authenticated, handlers.GetUserInfo)
+	users.Post("/login", handlers.Login)
+	users.Delete("/logout", handlers.Logout)
+
+	products := router.Group("/products", middleware.Authenticated)
+	products.Post("/", handlers.CreateProduct)
+	products.Post("/all", handlers.GetProducts)
+	products.Delete("/:id", handlers.DeleteProduct)
+	products.Post("/:id", handlers.GetProductById)
+	products.Put("/:id", handlers.UpdateProduct)
+
+	router.Use(func(c *fiber.Ctx) error {
+		return c.Status(404).JSON(fiber.Map{
+			"code":    404,
+			"message": "404: Not Found",
+		})
+	})
+
+}
