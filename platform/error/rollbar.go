111762e1ede8b7d3c5801a9ee393b1d5790b191f
BoYanZh <boyanzh233@gmail.com>
2023-01-28 03:19:49 +0800

    feat: unify rollbar logger

--- platform/error/rollbar.go
+++ platform/error/rollbar.go
@@ -3,8 +3,10 @@ package error
 import (
 	"github.com/joint-online-judge/go-horse/pkg/configs"
 	"github.com/rollbar/rollbar-go"
+	"github.com/sirupsen/logrus"
 )
 
 func ConnectRollbar() {
 	rollbar.SetToken(configs.Conf.RollbarAccessToken)
+	rollbar.SetLogger(logrus.StandardLogger())
 }
