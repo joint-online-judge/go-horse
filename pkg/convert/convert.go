ecf34985b8b788183eed7e4327c393082bf108b2
BoYanZh <boyanzh233@gmail.com>
2023-02-26 02:05:36 +0800

    fix: conver.To

--- pkg/convert/convert.go
+++ pkg/convert/convert.go
@@ -2,7 +2,7 @@ package convert
 
 import "github.com/jinzhu/copier"
 
-func To[DstType any](src any) (dst DstType, err error) {
-	err = copier.Copy(&src, &dst)
+func To[DstType any, SrcType any](src SrcType) (dst DstType, err error) {
+	err = copier.Copy(&dst, &src)
 	return
 }
